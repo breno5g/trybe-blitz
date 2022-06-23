f6838df572dada661c05de4bac590532c4d1fd75
Breno Santos <brenosantos145@gmail.com>
2022-06-23 09:21:44 -0300

    :sparkles: Create a seeder for tasks table
    Criação do seeder para a tabela de tarefas

---
+++ backend/src/database/seeders/20220623121156-tasks.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, _Sequelize) {
+    await queryInterface.bulkInsert('tasks', [
+      {
+        title: 'Jogar lil farm',
+        description: 'Encher o saco até ir jogar',
+        user_id: 1,
+        status: 'pending',
+        created_at: new Date()
+      },
+      {
+        title: 'Fiquei sem ideia',
+        description: 'aqui também',
+        user_id: 2,
+        status: 'in progress',
+        created_at: new Date()
+      },
+      {
+        title: 'Fiquei sem ideia',
+        description: 'aqui também',
+        user_id: 3,
+        status: 'done',
+        created_at: new Date()
+      },
+    ], {});
+  },
+
+  async down (queryInterface, _Sequelize) {
+
+    await queryInterface.bulkDelete('tasks', null, {});
+  }
+};
