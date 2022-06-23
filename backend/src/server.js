7245593d0d73beba01dee6e3ff972eeac27f155d
Breno Santos <brenosantos145@gmail.com>
2022-06-23 09:57:51 -0300

    :sparkles: Apply a user router
    Aplicando a rota de registro de usuario

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const routes = require('./routes');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  return res.status(200).json({ message: 3001 });
-});
-
 const PORT = process.env.PORT || 3001;
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log('Server on');
