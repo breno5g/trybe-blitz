2bf992c2e8002c3df126f37b17b5d0a5d57a03f1
Breno Santos <brenosantos145@gmail.com>
2022-06-24 08:56:57 -0300

    :sparkles: Implement login function in controller to pass test
    Implementando a primeira parte da função de login na controller para passar no test

--- backend/src/controllers/user.controller..js
+++ backend/src/controllers/user.controller..js
@@ -2,14 +2,23 @@ const service = require('../services/user.service');
 
 const create = async (req, res, next) => {
   try {
-    const {username, email, password} = req.body;
-    await service.create({username, email, password});
-    res.status(201).json({message: 'user created successfully'});
+    const { username, email, password } = req.body;
+    await service.create({ username, email, password });
+    res.status(201).json({ message: 'user created successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    console.log();
   } catch (error) {
     next(error);
   }
 };
 
 module.exports = {
-  create
+  create,
+  login,
 };
