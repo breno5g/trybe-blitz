067399cf78bd8d2eb5c73d5f71a250ba65992d74
Breno Santos <brenosantos145@gmail.com>
2022-06-24 08:49:19 -0300

    :closed_lock_with_key: Update user passwords in seeders
    Atualizando as senhas dos usuarios nos seeders para a versão encriptada

--- backend/src/database/seeders/20220623120708-users.js
+++ backend/src/database/seeders/20220623120708-users.js
@@ -1,27 +1,37 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, _Sequelize) {
-    await queryInterface.bulkInsert('users', [
-      {
-        username: 'bola8',
-        email: 'bola8@gmail.com',
-        password: 'vaiRolarLilFarm?'
-      },
-      {
-        username: 'msHairyHeart',
-        email: 'msHairyHeart@gmail.com',
-        password: 'msHairyHeart'
-      },
-      {
-        username: 'irene',
-        email: 'irene@gmail.com',
-        password: 'umaSenhoraJovem'
-      },
-    ], {});
+  async up(queryInterface, _Sequelize) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          username: 'bola8',
+          email: 'bola8@gmail.com',
+          // password: 'vaiRolarLilFarm?'
+          password:
+            '$2b$10$TqCPJYcoADrB2QJGHbVJ2OkzIS2klUnGc1a6vmkLESI9K7PVg.wf.',
+        },
+        {
+          username: 'msHairyHeart',
+          email: 'msHairyHeart@gmail.com',
+          // password: 'msHairyHeart',
+          password:
+            '$2b$10$lmLzdoBNZ6qeWgBSN1Q/SuGMr9zESfvjAVasBm6dqksjyraApoLje',
+        },
+        {
+          username: 'irene',
+          email: 'irene@gmail.com',
+          // password: 'umaSenhoraJovem',
+          password:
+            '$2b$10$QMlUJI2w1.sI1wHJR1Eu0e8FJrWt5vJZ.ILRWZS3mYatgl2Wlk19i',
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, _Sequelize) {
+  async down(queryInterface, _Sequelize) {
     await queryInterface.bulkDelete('users', null, {});
-  }
+  },
 };
