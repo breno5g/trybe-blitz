e8f96bed741ff019bce9e96a13d23f58c75c1f33
Breno Santos <brenosantos145@gmail.com>
2022-06-27 08:53:08 -0300

    :sparkles: Implement validation token middleware
    Implentando um middleware de validação do token

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('../controllers/task.controller');
+const validateToken = require('../middlewares/validateToken.middleware');
 
-router.get('/', controller.getAll);
+router.get('/', validateToken, controller.getAll);
 // router.post('/register', controller.create);
 
 module.exports = router;
