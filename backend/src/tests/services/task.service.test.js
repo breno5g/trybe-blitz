b663bba8926cf5ae490f79a49fe1325594903c1c
Breno Santos <brenosantos145@gmail.com>
2022-06-27 09:06:22 -0300

    :white_check_mark: Implement create task test
    Implementando a primeira versão do teste de criação de novas tasks

--- backend/src/tests/services/task.service.test.js
+++ backend/src/tests/services/task.service.test.js
@@ -1,8 +1,8 @@
-const { user } = require('../../database/models');
+const { user, task } = require('../../database/models');
 const service = require('../../services/task.service');
 const dataMock = require('../mocks/data.mock');
 
-describe('Get All tasks service tests', () => {
+describe('Get All tasks - service test', () => {
   beforeAll(() => {
     user.findAll = jest.fn().mockReturnValue(dataMock);
   });
@@ -16,5 +16,21 @@ describe('Get All tasks service tests', () => {
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxMCwidXNlcm5hbWUiOiJ0ZXN0ZSIsImVtYWlsIjoidGVzdGVAdGVzdGUuY29tIn0sImlhdCI6MTY1NjMyODYyOSwiZXhwIjoxNjU2OTMzNDI5fQ.a0QMCCbl3aJYZykj4PLqjaihNT5dY4QZHVTZD6ELj1I'
     );
     expect(tasks).toEqual(dataMock);
+  });
+});
+
+describe('Create task - service test', () => {
+  describe('Success case', () => {
+    beforeAll(() => {
+      task.create = jest.fn();
+    });
+
+    afterAll(() => {
+      task.create.mockReset();
+    });
+    test('task created successfully', async () => {
+      await service.create();
+      expect(task.create).toHaveBeenCalled();
+    });
   });
 });
