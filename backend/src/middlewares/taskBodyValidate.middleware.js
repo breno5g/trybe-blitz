f4754eb04d7546231f408bf6bee50ffc83d13650
Breno Santos <brenosantos145@gmail.com>
2022-06-29 11:26:51 -0300

    :sparkles: Add fail case in controller

--- backend/src/middlewares/taskBodyValidate.middleware.js
+++ backend/src/middlewares/taskBodyValidate.middleware.js
@@ -1,21 +1,21 @@
 const { create } = require('../schemas/task');
-const JWT = require('../utils/jwt.class');
+// const JWT = require('../utils/jwt.class');
 
 const taskBodyValidate = (req, res, next) => {
   const { title, description, status, userId } = req.body;
-  const { authorization } = req.headers;
-  const id = new JWT().validateToken(authorization).data.id;
+  // const { authorization } = req.headers;
+  // const id = new JWT().validateToken(authorization).data.id;
 
   const { error } = create.validate({ title, description, status, userId });
   if (error) {
     next({ status: 400, message: error.message });
   }
-  if (id !== userId) {
-    next({
-      status: 401,
-      message: 'You are not allowed to create tasks on other users',
-    });
-  }
+  // if (id !== userId) {
+  //   next({
+  //     status: 401,
+  //     message: 'You are not allowed to create tasks on other users',
+  //   });
+  // }
 
   next();
 };
--- backend/src/tests/controllers/task.controller.test.js
+++ backend/src/tests/controllers/task.controller.test.js
@@ -1,6 +1,7 @@
 const service = require('../../services/task.service');
 const controller = require('../../controllers/task.controller');
-// const MyError = require('../../utils/error.class');
+const MyError = require('../../utils/error.class');
+
 const {
   mockRequest,
   mockResponse,
@@ -77,34 +78,39 @@ describe('Delete a task - controller test', () => {
 });
 
 describe('Edit a task - controller test', () => {
-  // describe('Fail case', () => {
-  //   let req = mockRequest();
-  //   let res = mockResponse();
-  //   let next = mockNext();
-  //   beforeAll(() => {
-  //     service.update = jest.fn();
-  //     req.body = {
-  //       userId: 10,
-  //       title: 'teste',
-  //       description: 'teste',
-  //       status: 'pending',
-  //     };
-  //     req.headers.authorization =
-  //       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6InRlc3RlIiwiZW1haWwiOiJ0ZXN0ZUB0ZXN0ZS5jb20ifSwiaWF0IjoxNjU2NTA4ODM2LCJleHAiOjg2NTY1NjUwODgzNn0.gzgkC3DQUQFqgpsnPH_s12BNrvoBhcYlZ7MAnWd8Qio';
-  //   });
+  describe('Fail case', () => {
+    let req = mockRequest();
+    let res = mockResponse();
+    let next = mockNext();
+    beforeAll(() => {
+      service.update = jest.fn().mockImplementation(() => {
+        throw new MyError(
+          401,
+          'You are not allowed to update tasks on other users'
+        );
+      });
+      req.body = {
+        userId: 10,
+        title: 'teste',
+        description: 'teste',
+        status: 'pending',
+      };
+      req.headers.authorization =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6InRlc3RlIiwiZW1haWwiOiJ0ZXN0ZUB0ZXN0ZS5jb20ifSwiaWF0IjoxNjU2NTA4ODM2LCJleHAiOjg2NTY1NjUwODgzNn0.gzgkC3DQUQFqgpsnPH_s12BNrvoBhcYlZ7MAnWd8Qio';
+    });
 
-  //   afterAll(() => {
-  //     service.update = jest.fn();
-  //   });
+    afterAll(() => {
+      service.update = jest.fn();
+    });
 
-  //   test('Can update a task', async () => {
-  //     try {
-  //       await controller.update(req, res, next);
-  //     } catch (error) {
-  //       expect(next).toHaveBeenCalled();
-  //     }
-  //   });
-  // });
+    test('Can update a task', async () => {
+      try {
+        await controller.update(req, res, next);
+      } catch (error) {
+        expect(next).toHaveBeenCalled();
+      }
+    });
+  });
 
   describe('Success case', () => {
     let req = mockRequest();
