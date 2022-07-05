b65b5846528a17e48d7ddce7e2b96fac3615e0ca
Breno Santos <brenosantos145@gmail.com>
2022-07-05 17:30:09 -0300

    update seeders

--- backend/src/database/seeders/20220623120708-users.js
+++ backend/src/database/seeders/20220623120708-users.js
@@ -6,25 +6,11 @@ module.exports = {
       'users',
       [
         {
-          username: 'bola8',
-          email: 'bola8@gmail.com',
+          username: 'teste',
+          email: 'teste@gmail.com',
           // password: 'vaiRolarLilFarm?'
           password:
             '$2b$10$TqCPJYcoADrB2QJGHbVJ2OkzIS2klUnGc1a6vmkLESI9K7PVg.wf.',
-        },
-        {
-          username: 'msHairyHeart',
-          email: 'msHairyHeart@gmail.com',
-          // password: 'msHairyHeart',
-          password:
-            '$2b$10$lmLzdoBNZ6qeWgBSN1Q/SuGMr9zESfvjAVasBm6dqksjyraApoLje',
-        },
-        {
-          username: 'irene',
-          email: 'irene@gmail.com',
-          // password: 'umaSenhoraJovem',
-          password:
-            '$2b$10$QMlUJI2w1.sI1wHJR1Eu0e8FJrWt5vJZ.ILRWZS3mYatgl2Wlk19i',
         },
       ],
       {}
